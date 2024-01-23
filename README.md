5ead22ebb2760bb2b5c03544040aa4e98e71d683
Chris Gianelloni <wolf31o2@blinklabs.io>
2024-01-23 08:24:17 -0500

    feat: initial simple webhook receiver (#1)
    Signed-off-by: Chris Gianelloni <wolf31o2@blinklabs.io>

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # snek-webhook-starter-kit
+
+```bash
+snek --input-chainsync-include-cbor --output=webhook --output-webhook-url=http://localhost:8080
+```
---
+++ main.go
@@ -0,0 +1,51 @@
+// Copyright 2023 Blink Labs Software
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func handleWebhook(c *gin.Context) {
+	if c.ContentType() != "application/json" {
+		c.JSON(
+			http.StatusUnsupportedMediaType,
+			"invalid request body, should be application/json",
+		)
+		return
+	}
+	rawBytes, err := io.ReadAll(c.Request.Body)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, "failed to read request body")
+		return
+	}
+	_ = c.Request.Body.Close()
+	// fmt.Printf("received webhook payload: %s\n", string(rawBytes))
+	fmt.Println(string(rawBytes))
+}
+
+func main() {
+	gin.SetMode(gin.ReleaseMode)
+	gin.DisableConsoleColor()
+	router := gin.New()
+	router.POST("/", handleWebhook)
+	_ = router.Run()
+	// fmt.Println("started webhook server")
+	select {}
+}
